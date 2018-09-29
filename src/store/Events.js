4142a7089539a4ae43006d8c5f1976d232bca523
Daniel Molin <dkd333@gmail.com>
2018-09-29 19:03:47 +0200

    fix soundboard file loading

--- src/store/Events.js
+++ src/store/Events.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import api from "../api";
 
+import honk from  '../assets/sounds/honk.wav';
+import applause from  "../assets/sounds/applause.wav";
+import orgasm from  "../assets/sounds/orgasm.wav";
+import whistle from  "../assets/sounds/whistle.wav";
+import yeah from  "../assets/sounds/yeah.wav";
+import wilhelm from  "../assets/sounds/wilhelm.wav";
+import airhorn from  "../assets/sounds/airhorn.wav";
+import brrrap from  "../assets/sounds/brrrap.wav";
+import rastafari from  "../assets/sounds/rastafari.wav";
+
+
 import {
     Howler,
     Howl
@@ -10,15 +21,15 @@ from 'howler';
 Howler.volume(1);
 
 var sounds = {
-    'honk': ['sounds/honk.wav'],
-    'applause': ["sounds/applause.wav"],
-    'orgasm': ["sounds/orgasm.wav"],
-    'whistle': ["sounds/whistle.wav"],
-    'yeah': ["sounds/yeah.wav"],
-    'wilhelm': ["sounds/wilhelm.wav"],
-    'airhorn': ["sounds/airhorn.wav"],
-    'brrrap': ["sounds/brrrap.wav"],
-    'rastafari': ["sounds/rastafari.wav"],
+    'honk': [honk],
+    'applause': [applause],
+    'orgasm': [orgasm],
+    'whistle': [whistle],
+    'yeah': [yeah],
+    'wilhelm': [wilhelm],
+    'airhorn': [airhorn],
+    'brrrap': [brrrap],
+    'rastafari': [rastafari],
 
 };
 let howls = {};
@@ -43,33 +54,40 @@ export default {
 
     mutations: {
         SOCKET_EVENT(state, event) {
-            state.event = event;
-            state.events.push(event);
-
-            switch (event.type) {
-                case "waitingForNextSong":
-                    this.dispatch("Messages/toast", {
-                        type: "deepsea",
-                        html: "Next song: <b>" + event.data.track.name + "</b>",
-                        duration: 10
-                    });
-                    break;
-                case "playMySong":
-                    this.dispatch("Messages/popup", {
-                        type: "deepsea",
-                        html: "<p>Playing your song:</p>" +
-                            "<p><b>" + event.data.track.name + "</b></p>" +
-                            "<p>(" + event.data.track.artists[0].name + ")</p>",
-                        duration: 10,
-                    });
-                    break;
-                case "playSound":
-                    let sound = howls[event.data.sound];
-                    if (sound) {
-                        sound.play();
-                    }
-                    break;
+            if(!( event instanceof Array)){
+                event = [event];
             }
+
+            event.forEach(_event => {
+                state.event = _event;
+                state.events.push(_event);
+    
+                switch (_event.type) {
+                    case "waitingForNextSong":
+                        this.dispatch("Messages/toast", {
+                            type: "deepsea",
+                            html: "Next song: <b>" + _event.data.track.name + "</b>",
+                            duration: 10
+                        });
+                        break;
+                    case "playMySong":
+                        this.dispatch("Messages/popup", {
+                            type: "deepsea",
+                            html: "<p>Playing your song:</p>" +
+                                "<p><b>" + _event.data.track.name + "</b></p>" +
+                                "<p>(" + _event.data.track.artists[0].name + ")</p>",
+                            duration: 10,
+                        });
+                        break;
+                    case "playSound":
+                        let sound = howls[_event.data.sound];
+                        if (sound) {
+                            sound.play();
+                        }
+                        break;
+                }
+            });
+
         },
     },
 }
