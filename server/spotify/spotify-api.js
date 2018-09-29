d995b76457bcdbb0fe72b7c8571cd14df0be2f85
Daniel Molin <dkd333@gmail.com>
2018-09-29 17:07:03 +0200

    Add server src

---
+++ server/spotify/spotify-api.js
@@ -0,0 +1,52 @@
+const SpotifyWebApi = require("spotify-web-api-node");
+
+let config = {};
+
+try {
+    config = require("../../../snoppify-config.js");
+
+    if (config.client_id && config.client_secret) {
+        config.auth_token = new Buffer(config.client_id + ":" + config.client_secret).toString('base64');
+    }
+} catch (ex) {
+    console.log("No snoppify config file");
+}
+
+const api = new SpotifyWebApi({
+    redirectUri: "http://192.168.57.201:3000/refresh-token",
+    clientId: config.client_id,
+    clientSecret: config.client_secret,
+});
+
+api.config = config;
+
+api.onload = new Promise(function(resolve, reject) {
+
+    api.clientCredentialsGrant().then(function(data) {
+        // Save the access token so that it's used in future calls
+        api.setAccessToken(data.body['access_token']);
+
+        refreshAccessToken();
+
+        resolve();
+    }, function(err) {
+        console.log('Something went wrong when retrieving an access token', err);
+
+        reject();
+    });
+
+});
+
+function refreshAccessToken() {
+    setInterval(function() {
+        api.clientCredentialsGrant().then(function(data) {
+            console.log("Updated access_token:", data.body.access_token);
+            // Save the access token so that it's used in future calls
+            api.setAccessToken(data.body.access_token);
+        }, function(err) {
+            console.log('Something went wrong when retrieving an access token', err);
+        });
+    }, 3000 * 1000);
+}
+
+module.exports = api;
