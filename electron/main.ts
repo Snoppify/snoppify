b554c8f374bc6730fa8e77768f4919a1618d45d4
Daniel Molin <daniel.molin@getcompliant.org>
2019-07-22 22:23:48 +0200

    Add Electron base

---
+++ electron/main.ts
@@ -0,0 +1,19 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+    // Create the browser window.
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+
+        },
+        backgroundColor: "#181818",
+
+    });
+
+    // and load the index.html of the app.
+    win.loadFile("index.html");
+}
+
+app.on("ready", createWindow);
