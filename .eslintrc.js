e38805f1ffd8af81664bb4167de5f271cb6873d7
Daniel Molin <fullname.daniel@gmail.com>
2022-04-12 14:42:32 +0200

    Add some eslint rules and configs and fix for Vue maybe

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,16 @@ module.exports = {
   },
   ignorePatterns: ["dist/", "data/", "server-dist/"],
   extends: [
-    "plugin:vue/essential",
     "eslint:recommended",
+    "plugin:vue/essential",
     "@vue/prettier",
     "airbnb-base",
     "airbnb-typescript/base",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     project: ["./tsconfig.json", "./tsconfig.server.json"],
+    extraFileExtensions: [".vue"],
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -26,12 +28,22 @@ module.exports = {
     "@typescript-eslint/quotes": ["error", "double"],
     "linebreak-style": "off", // handled as "auto" by prettier
     "no-underscore-dangle": "off",
+    "no-else-return": "off",
   },
   overrides: [
     {
       files: ["*.ts"],
       parser: "@typescript-eslint/parser",
       plugins: ["@typescript-eslint"],
+    },
+    {
+      files: ["*.vue"],
+      parser: "vue-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+        extraFileExtensions: [".vue"],
+      },
     },
   ],
 };
