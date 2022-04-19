754c629ee72689ce7897c6bf6fe8ec206fcdc4b0
Daniel Molin <dkd333@gmail.com>
2022-04-19 11:06:24 +0200

    Correctly split eslint config into client/server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,72 @@
+/**
+ * Rules common for both client (Vue) and server (TS)
+ */
+var commonRules = {
+  "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+  "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+  "no-unused-vars": [
+    "warn",
+    { vars: "all", args: "after-used", ignoreRestSiblings: true },
+  ],
+  "linebreak-style": "off", // handled as "auto" by prettier
+  "no-underscore-dangle": "off",
+  "no-else-return": "off",
+  eqeqeq: "warn",
+  "prefer-destructuring": ["error", { object: true, array: false }],
+  "no-restricted-syntax": "off",
+  "no-return-assign": ["error", "except-parens"],
+  "import/prefer-default-export": "off",
+  "no-plusplus": "off",
+  "no-cond-assign": ["error", "except-parens"],
+};
+
 module.exports = {
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ["dist/", "data/", "server-dist/", "electron/"],
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/essential",
-    "@vue/prettier",
-    "airbnb-base",
-    "airbnb-typescript/base",
-    "plugin:prettier/recommended",
+  ignorePatterns: [
+    "dist/",
+    "data/",
+    "server-dist/",
+    "electron/",
+    "electron-dist/",
   ],
-  parserOptions: {
-    project: ["./tsconfig.json", "./tsconfig.server.json"],
-    extraFileExtensions: [".vue"],
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-unused-vars": [
-      "warn",
-      { vars: "all", args: "after-used", ignoreRestSiblings: true },
-    ],
-    "vue/multi-word-component-names": "off",
-    "@typescript-eslint/quotes": ["error", "double"],
-    "linebreak-style": "off", // handled as "auto" by prettier
-    "no-underscore-dangle": "off",
-    "no-else-return": "off",
-    eqeqeq: "warn",
-    "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
-    "prefer-destructuring": ["error", { object: true, array: false }],
-    "no-restricted-syntax": "off",
-    "no-return-assign": ["error", "except-parens"],
-    "import/prefer-default-export": "off",
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "variable",
-        modifiers: ["destructured"],
-        format: null,
-      },
-    ],
-    "no-plusplus": "off",
-    "no-cond-assign": ["error", "except-parens"],
-  },
   overrides: [
     {
-      files: ["*.ts"],
+      files: ["server/**/*"],
       parser: "@typescript-eslint/parser",
       plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "airbnb-base",
+        "airbnb-typescript/base",
+        "plugin:prettier/recommended",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.server.json"],
+      },
+      rules: Object.assign({}, commonRules, {
+        "@typescript-eslint/quotes": ["error", "double"],
+        "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {
+            selector: "variable",
+            modifiers: ["destructured"],
+            format: null,
+          },
+        ],
+      }),
     },
     {
-      files: ["*.vue"],
+      files: ["src/**/*"],
       parser: "vue-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        sourceType: "module",
-        extraFileExtensions: [".vue"],
-      },
+      extends: ["eslint:recommended", "plugin:vue/essential", "@vue/prettier"],
+      rules: Object.assign({}, commonRules, {
+        "vue/multi-word-component-names": "off",
+      }),
     },
   ],
 };
