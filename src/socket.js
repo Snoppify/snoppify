a4580b3a825f61e71de5858571170f77961fe073
Daniel Molin <dkd333@gmail.com>
2022-09-05 21:39:24 +0200

    fix socket bug after updates

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,13 @@
+import io from "socket.io-client";
 import Vue from "vue";
 import VueSocketio from "vue-socket.io";
 
 import { store } from "./store";
 
+const socketInstance = io.connect(process.env.VUE_APP_SERVER_URI);
+
 export default {
   init: () => {
-    Vue.use(VueSocketio, process.env.VUE_APP_SERVER_URI, store);
+    Vue.use(new VueSocketio({ connection: socketInstance, store }));
   },
 };
