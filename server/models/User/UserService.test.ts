74777bae2d0311aa98c64ddde65f0f5d910a024e
Daniel Molin <fullname.daniel@gmail.com>
2022-09-05 15:28:11 +0200

    UserService: add queue operations  - addToQueue  - removeFromQueue

--- server/models/User/UserService.test.ts
+++ server/models/User/UserService.test.ts
@@ -8,6 +8,9 @@ describe("UserService", () => {
   beforeEach(() => {
     // Clear all instances and calls to constructor and all methods:
     (UserRepository as jest.Mock).mockClear();
+    jest
+      .spyOn(UserRepository.prototype, "upsave")
+      .mockImplementation(() => Promise.resolve());
     userService.setRepository(new UserRepository());
   });
 
@@ -23,6 +26,30 @@ describe("UserService", () => {
     expect(
       (UserRepository as jest.Mock).mock.instances[0].upsave,
     ).toHaveBeenCalledWith(new User(minimalUserData()));
+  });
+
+  it("adds a track to the queue", async () => {
+    const user = new User(minimalUserData());
+
+    const updatedUser = await userService.addToQueue(user, {
+      id: "new_track_id",
+    });
+
+    expect(updatedUser.queue.getAt(0)).toEqual({ id: "new_track_id" });
+  });
+
+  it("removes a track from the queue", async () => {
+    const user = new User({
+      ...minimalUserData(),
+      queue: [{ id: "track_id" }],
+    });
+
+    expect(user.queue.size).toBe(1);
+
+    const updatedUser = await userService.removeFromQueue(user, {
+      id: "track_id",
+    });
+    expect(updatedUser.queue.size).toBe(0);
   });
 });
 
--- server/models/User/UserService.ts
+++ server/models/User/UserService.ts
@@ -1,9 +1,17 @@
+import { Queue } from "../Queue/Queue";
 import { UserBase } from "./UserBase";
 import { UserRepository } from "./UserRepository";
 
 let repo: UserRepository;
 
-export const userService = { getUser, upsave, setRepository, getAll };
+export const userService = {
+  getUser,
+  upsave,
+  setRepository,
+  getAll,
+  addToQueue,
+  removeFromQueue,
+};
 
 function getUser(id: string) {
   return repo.get(id);
@@ -20,3 +28,16 @@ function setRepository(newRepo: UserRepository) {
 function getAll() {
   return repo.getAll();
 }
+
+// TODO: Write tests THEn implement! then replace direct modification of User.queue!
+function addToQueue(user: UserBase, track: { id: string }): Promise<UserBase> {
+  (user.queue as Queue<{ id: string }>).add(track);
+  return userService.upsave(user).then(() => user);
+}
+function removeFromQueue(user: UserBase, track: { id: string }) {
+  (user.queue as Queue<{ id: string }>).remove(track);
+  return upsave(user).then(() => Promise.resolve(user));
+}
+// function clearQueue() {}
+// function clearUser() {}
+// function toggleVoteOnTrack(opts: { voter; votee; track: { id: string } }) {}
